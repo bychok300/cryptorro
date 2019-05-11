5ffef2d82bd8e3ec66ff5f30cecc7b8e49ab0375
Anton Bychek <bychok300@inbox.ru>
2019-05-11 22:00:40 +0300

    a bit refactor

--- src/AESCipher.py
+++ src/AESCipher.py
@@ -4,10 +4,11 @@ from getpass import getpass
 from Crypto import Random
 from Crypto.Cipher import AES
 
+from FileUtils import FileUtils
+
 
 class AESCipher(object):
-
-    key = getpass(prompt='Encryption key: ', stream=None)
+    key = getpass(prompt='Encryption key: ')
 
     def __init__(self):
         self.bs = 32
@@ -30,4 +31,26 @@ class AESCipher(object):
 
     @staticmethod
     def _unpad(s):
-        return s[:-ord(s[len(s)-1:])]
+        return s[:-ord(s[len(s) - 1:])]
+
+    @staticmethod
+    def encrypt_files_recurcive(options):
+        files = FileUtils.recursiv_list_files_in(options.path)
+        for file in files:
+            if options.encrypt:
+                try:
+                    FileUtils.replace_file_content(file, AESCipher().encrypt(FileUtils.read_file(file)))
+                except:
+                    print("Error in " + file + " possibly can not encrypt file")
+            else:
+                try:
+                    FileUtils.replace_file_content(file, AESCipher().decrypt(FileUtils.read_file(file)))
+                except:
+                    print("Error in " + file + " possibly can not decrypt file")
+
+    @staticmethod
+    def encrypt_file(options):
+        try:
+            FileUtils.replace_file_content(options.file, AESCipher().encrypt(FileUtils.read_file(options.file)))
+        except:
+            print("Error in " + options.file + " possibly can not decrypt file")
--- src/cryptorro.py
+++ src/cryptorro.py
@@ -1,10 +1,10 @@
 import argparse
-
 import sys
 
 parser = argparse.ArgumentParser(description='Encryption tool for encrypting whole files in folders')
 
 parser.add_argument("-p", dest="path", type=str, help="Path to folder.")
+parser.add_argument("-f", dest="file", type=str, help="Path to file.")
 parser.add_argument("-e", dest="encrypt", action="store_true", help="Encrypt every file in folder")
 parser.add_argument("-d", dest="decrypt", action="store_true", help="Decrypt every file in folder")
 
@@ -14,19 +14,10 @@ if len(sys.argv) < 4:
     parser.print_help()
     sys.exit(1)
 
-# stupid hack that allow call getPass only when num of args is valid
+# stupid hack to avoid call getPass only when num of args is valid
 from AESCipher import AESCipher
-from FileUtils import FileUtils
 
-files = FileUtils.recursiv_list_files_in(options.path)
-for file in files:
-    if options.encrypt:
-        try:
-            FileUtils.replace_file_content(file, AESCipher().encrypt(FileUtils.read_file(file)))
-        except:
-            print("Error in " + file + " possibly can not encrypt file")
-    else:
-        try:
-            FileUtils.replace_file_content(file, AESCipher().decrypt(FileUtils.read_file(file)))
-        except:
-            print("Error in " + file + " possibly can not decrypt file")
+if options.file is not None:
+    AESCipher.encrypt_file(options)
+else:
+    AESCipher.encrypt_files_recurcive(options)
